0fc8c5d0a50b0987ea91ef33be746e9c18e99fbf
omprakash-utl <omprakash.k@utahtechlab.com>
2023-04-19 14:13:57 +0530

    add new design

--- src/app/layouts/customer-layout/customer-layout.module.ts
+++ src/app/layouts/customer-layout/customer-layout.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerLayoutRoutingModule } from './customer-layout-routing.module';
 import { CustomerComponent } from './customer/customer.component';
-
+import { CustomerHeaderComponent } from 'app/shared/customer-header/customer-header.component';
+import { CustomerFooterComponent } from 'app/shared/customer-footer/customer-footer.component';
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    CustomerHeaderComponent,
+    CustomerFooterComponent
   ],
   imports: [
     CommonModule,
